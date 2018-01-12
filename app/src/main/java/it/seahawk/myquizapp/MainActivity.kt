850e96f6eb102616f1688d6183b633e2319c553b
itchrand <ac_is_nowhere@yahoo.com>
2018-01-12 12:01:59 +0100

    Add questions 1 to 3 in layout. add Toast function.

--- app/src/main/java/it/seahawk/myquizapp/MainActivity.kt
+++ app/src/main/java/it/seahawk/myquizapp/MainActivity.kt
@@ -3,6 +3,7 @@ package it.seahawk.myquizapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +16,18 @@ class MainActivity : AppCompatActivity() {
      * This method is called when the submit button is clicked.
      */
     fun submitAnswers(view: View) {
+       giveAToast("submitted")
 
+    }
 
+    /**
+     * This method gives a toast message.
+     */
+    private fun giveAToast(message: String) {
+        val context = applicationContext
+        val duration = Toast.LENGTH_SHORT
+
+        val toast = Toast.makeText(context, message, duration)
+        toast.show()
     }
 }
